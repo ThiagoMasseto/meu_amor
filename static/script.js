db748b85ebfd8f02eb887eaa7aebce9b5ff52bae
ThiagoMasseto <thimasseto@gmail.com>
2026-06-12 23:40:52 -0300

    começando o desenvolvimento do site para a gabi.

---
+++ static/script.js
@@ -0,0 +1,53 @@
+const heartsContainer = document.querySelector(".hearts");
+
+
+
+function createHeart(){
+
+
+    const heart = document.createElement("div");
+
+
+    heart.classList.add("heart");
+
+
+    heart.innerHTML = "❤️";
+
+
+
+    heart.style.left = Math.random() * 100 + "%";
+
+
+
+    heart.style.animationDuration =
+
+    Math.random() * 5 + 5 + "s";
+
+
+
+    heart.style.fontSize =
+
+    Math.random() * 20 + 15 + "px";
+
+
+
+    heartsContainer.appendChild(heart);
+
+
+
+    setTimeout(()=>{
+
+
+        heart.remove();
+
+
+    },8000)
+
+
+
+}
+
+
+
+setInterval(createHeart,300);
+s
